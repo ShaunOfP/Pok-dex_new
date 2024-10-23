2f924269e20b9df5a9146bd8878c1b1dd4eb626f
ShaunOfP <131476113+ShaunOfP@users.noreply.github.com>
2024-10-23 18:44:01 +0200

    added JSDoc; in commit before: restructured how the api requests are made

--- src/app/pokeapi.service.ts
+++ src/app/pokeapi.service.ts
@@ -14,16 +14,31 @@ export class PokeapiService {
 
   constructor(private http: HttpClient) { }
 
+
+  /**
+   * Retrieves the Pokemon Info with all the Details from the PokeAPI
+   * @param number A number
+   * @returns An Array with all the information about one Pokemon
+   */
   getPokemonInfoList(number: number): Observable<any> {
     return this.http.get(this.singleUrl + number);
   }
 
 
+  /**
+   * Retrieves the Evolution Lists from the PokeAPI
+   * @param evoCounter A number
+   * @returns An Array with all the Evolutions of the first Group
+   */
   getEvolutionInfoList(evoCounter: number): Observable<any> {
     return this.http.get(this.evoUrl + evoCounter);
   }
 
 
+  /**
+   * Retrieves the Color List from the PokeAPI
+   * @returns An Array which contains the Color-Code and all the matching Pokemon for this Color-Code
+   */
   getColorsOfPokemon(): Observable<any> {
     this.colorCounter++;
     return this.http.get(this.colorUrl + this.colorCounter);
